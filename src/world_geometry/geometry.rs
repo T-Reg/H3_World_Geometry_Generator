eff628d44a42d61e0719e1af39f427a69fd77704
MSI\timre <timregan10@gmail.com>
2025-07-21 22:01:27 -0700

    y is up

--- src/world_geometry/geometry.rs
+++ src/world_geometry/geometry.rs
@@ -2,10 +2,10 @@
 pub fn lat_lng_to_3d(lat_deg: f64, lng_deg: f64, radius: f64) -> [f32; 3] {
     let lat_rad = lat_deg.to_radians();
     let lng_rad = lng_deg.to_radians();
-    
+
     let x = radius * lat_rad.cos() * lng_rad.cos();
-    let y = radius * lat_rad.cos() * lng_rad.sin();
-    let z = radius * lat_rad.sin();
-    
+    let y = radius * lat_rad.sin(); // +Y is “up” in Godot
+    let z = radius * lat_rad.cos() * lng_rad.sin();
+
     [x as f32, y as f32, z as f32]
-} 
+}
